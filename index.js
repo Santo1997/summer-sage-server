2915278b8adcc78a07a4103356e484b923c53382
Santo1997 <hossainsanto2@gmail.com>
2023-06-09 17:04:52 +0600

    get courses

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 var cors = require("cors");
 const app = express();
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qpcvbrd.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,23 +19,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
-    // Send a ping to confirm a successful connection
+    const courseData = client.db("summerCamp").collection("courses");
+
+    app.get("/courses", async (req, res) => {
+      const result = await courseData.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    await client.close();
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello Summer Camp!");
+  res.send("Hello Summer Cump!");
 });
 
 app.listen(port, () => {
-  console.log(`Summer Camp listening on port ${port}`);
+  console.log(`Summer Cump listening on port ${port}`);
 });
